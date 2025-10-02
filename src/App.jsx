b5fa9932b7513e2166b87ccf68890148a3c1c373
Mohand Ashraf <mohanedashraf047@gmail.com>
2025-10-02 09:03:15 +0300

    fix BrowserRouter bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./components/css/all.min.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Shop from "./components/Shop";
 import Home from "./components/Home";
-import Blog from "./components/Blog"
-import About from './components/About'
-import Contact from './components/Contact'
-import Cart from './components/Cart'
+import Blog from "./components/Blog";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Cart from "./components/Cart";
 import CartProvider from "./components/Context/CartContext";
 import ScrollToTopButton from "./components/ScrollToTopButton";
 import ScrollToTop from "./components/ScrollToTop";
-import './components/css/all.min.css'
+import "./components/css/all.min.css";
 
 const App = () => {
-
-    const [randomColor, setRandomColor] = useState('');
+  const [randomColor, setRandomColor] = useState("");
 
   // Generate initial gradient on mount
   useEffect(() => {
@@ -31,24 +35,30 @@ const App = () => {
     return `linear-gradient(135deg, ${color1}, ${color2})`;
   };
 
-
   return (
-    <CartProvider> {/* ✅ Wrap everything in CartProvider */}
-      <Router>
-        <ScrollToTop />
-        <Navbar progressBarColor={randomColor}/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/cart" element={<Cart />} />
-          {/* <Route path="/product/:id" element={<ProductDetails />} /> */}
-        </Routes>
-        <ScrollToTopButton randomColor={randomColor} setRandomColor={() => setRandomColor(generateRandomColor())}/>
-        <Footer />
-      </Router>
+    <CartProvider>
+      {" "}
+      {/* ✅ Wrap everything in CartProvider */}
+      <BrowserRouter>
+        <Router>
+          <ScrollToTop />
+          <Navbar progressBarColor={randomColor} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+            {/* <Route path="/product/:id" element={<ProductDetails />} /> */}
+          </Routes>
+          <ScrollToTopButton
+            randomColor={randomColor}
+            setRandomColor={() => setRandomColor(generateRandomColor())}
+          />
+          <Footer />
+        </Router>
+      </BrowserRouter>
     </CartProvider>
   );
 };
