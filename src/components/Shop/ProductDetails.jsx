40bf0a7e547de1959c1697b1769fafdddb02fdf7
Mohand Ashraf <mohanedashraf047@gmail.com>
2025-10-16 20:41:01 +0300

    new-api

--- src/components/Shop/ProductDetails.jsx
+++ src/components/Shop/ProductDetails.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 import "../css/all.min.css";
 import "../css/hover-min.css";
-import { CartContext } from "../Context/CartContext";
-import { useState, useContext } from "react";
-// import { useState , useEffect} from "react";
-// import { useLocation } from "react-router-dom";
+import { useState} from "react";
+import { addToCart } from "../Redux/productSlice";
+import { useDispatch } from "react-redux";
 
 const ProductDetails = ({ product }) => {
-  const { addToCart } = useContext(CartContext);
   const [quantity, setQuantity] = useState(1);
-  // const [productImage, setProductImage] = useState(product?.images?.[0] || "");
-  // const [productImage, setProductImage] = useState(product?.img || "");
 
-  //  useEffect(() => {
-  //   if (product?.images?.length > 0) {
-  //     setProductImage(product.images[0]);
-  //   }
-  // }, [product]);
+  const dispatch = useDispatch();
+
+    const handleAddToCart = (product) => {
+      dispatch(addToCart({ ...product, quantity: quantity }));
+    };
 
   if (!product) {
     return (
@@ -29,7 +25,7 @@ const ProductDetails = ({ product }) => {
       <div className="single_pro_image">
         <div className="image-gallery">
           <img
-            src={product.img}
+            src={product.image}
             alt="Main"
             className="main-image"
             id="MainImage"
@@ -51,9 +47,9 @@ const ProductDetails = ({ product }) => {
       </div>
 
       <div className="single_pro_deatails">
-        <h6>Home / {product.title}</h6>
+        <h1> {product.category}</h1>
         <h4>{product.p}</h4>
-        <h2>{product.price}</h2>
+        <h2>${product.price}</h2>
         <select>
           <option>Select Size</option>
           <option>XL</option>
@@ -75,7 +71,7 @@ const ProductDetails = ({ product }) => {
           id="button"
           onClick={() => {
             // const qty = Number(document.getElementById("qtyInput").value) || 1;
-            addToCart({ ...product, quantity });
+            handleAddToCart(product);
           }}
         >
           Add To Cart
@@ -83,16 +79,7 @@ const ProductDetails = ({ product }) => {
         <h4>Product Details </h4>
 
         <span>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint id
-          beatae veritatis dolor pariatur iusto autem quod dolore, quaerat
-          assumenda itaque sed quos corporis a, enim saepe expedita officia
-          delectus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-          id beatae veritatis dolor pariatur iusto autem quod dolore, quaerat
-          assumenda itaque sed quos corporis a, enim saepe expedita officia
-          delectus.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-          id beatae veritatis dolor pariatur iusto autem quod dolore, quaerat
-          assumenda itaque sed quos corporis a, enim saepe expedita officia
-          delectus.
+          {product.description}
         </span>
       </div>
     </section>
